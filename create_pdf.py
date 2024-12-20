016d436ade46417228719aa3406d9d766c19c6e4
Gustavo Aranda <138258786+Gustavo-Aranda@users.noreply.github.com>
2024-12-20 11:10:39 -0300

    Plot do projeto

---
+++ create_pdf.py
@@ -0,0 +1,106 @@
+from fpdf import FPDF
+import dataframe_image as dfi
+import time
+
+
+# função para o título
+def titulo(pdf, title):
+
+    # titulo principal
+    pdf.set_font('Helvetica', 'b', 20)
+    pdf.ln(10)
+    pdf.write(5, title)
+    pdf.ln(10)
+
+    # adicionando a data de geração do relatório
+    pdf.set_font('Helvetica', '', 14)
+    pdf.set_text_color(r=128, g=128, b=128)
+    today = time.strftime("%d/%m/%Y")
+    pdf.write(4, f'{today}')
+
+    # add line break
+    pdf.ln(10)
+
+# função para o texto
+def texto(pdf, txt):
+
+    # ajustando cor de texto, fonte e espaçamento
+    pdf.set_text_color(0)
+    pdf.set_font('Helvetica', '', 12)
+
+    pdf.write(5, txt)
+    
+
+# variáveis globais
+WIDTH = 210
+HEIGHT = 297
+
+# criando o documento PDF
+pdf = FPDF() # A4 (210 x 297 mm)
+
+# --------  página 1 --------  #
+pdf.add_page()
+
+# título
+titulo(pdf, "Relatório do Desempenho de Vendas (2023)")
+
+# título da tabela
+texto(pdf, "1. Tabela das Vendas por mês da empresa em 2023")
+pdf.ln(10)
+
+# adicionando a tabela de vendas
+pdf.image("tab_vendas_por_mes.png", WIDTH/2 - 20, w=40)
+pdf.ln(20)
+
+# adicionando o gráfico de vendas
+pdf.image("vendas_por_mes.png", 10, 150, WIDTH - 20)
+pdf.ln(90)
+
+# texto explicativo
+texto(pdf, "A visualização acima mostra a tendência das vendas da empresa durante o ano de 2023. Conseguimos notar " \
+           "que em 5 meses no ano as vendas foram acima de 600 mil reais. Julho foi o mês com menor desempenho.")
+
+
+# --------  página 2 --------  #
+pdf.add_page()
+
+# título da tabela 2
+pdf.ln(20)
+texto(pdf, "2. Tabela dos Lucros por Departamento em 2023")
+pdf.ln(10)
+
+# adicionando a tabela de lucro
+pdf.image("tab_lucro_dpt.png", WIDTH/2 - 40, w=80)
+pdf.ln(20)
+
+# adicionando o gráfico de lucro
+pdf.image("lucro_dpt.png", 10, WIDTH/2, WIDTH - 20)
+pdf.ln(120)
+
+# texto explicativo
+texto(pdf, "O departamento que apresenta o maior lucro da empresa é o Automotivo. O resultado é esperado, visto que grande" \
+           " parte dos produtos automotivos estão entre os mais caros por unidade. Na sequência temos os produtos de Jardinagem e" \
+           " paisagismo, por fim, os materiais de construção com menos de R$ 30 mil de lucro em 2023.")
+
+# --------  página 3 --------  #
+pdf.add_page()
+
+# título da tabela 3
+pdf.ln(20)
+texto(pdf, "3. Tabela da quantidade de produtos por Modo de Envio em São Paulo (2023)")
+pdf.ln(10)
+
+# adicionando a tabela de modo de envio
+pdf.image("tab_envio_sp.png", WIDTH/2 - 40, w=60)
+pdf.ln(20)
+
+# adicionando o gráfico de modo de envio
+pdf.image("envio_sp.png", 10, WIDTH/2, WIDTH - 20)
+pdf.ln(120)
+
+# texto explicativo
+texto(pdf, "O modo de envio mais utilizado de forma disparada é a Entrega Padrão, corrrespondendo a quase 60% de todas as entregas em" \
+           " São Paulo. Em seguida, temos a Econômica e Envio rápido e, por último, a de 24 horas com menos de 10% de entregas no estado.")
+
+# gerando o PDF
+pdf.output("Relatório 2023.pdf", 'F')
---
+++ main.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from fpdf import FPDF
+import dataframe_image as dfi
+
+
+
+vendas = pd.read_csv("vendas.csv")
+vendas["data_pedido"] = pd.to_datetime(vendas["data_pedido"], format="%Y-%m-%d") #forçando todas as datas para este formato
+#print(vendas)
+
+
+
+vendas_por_mes = vendas[["data_pedido","vendas"]] #criando a tabela de vendas por mes a partir da coluna data_pedido e as vendas
+vendas_por_mes.set_index("data_pedido", inplace=True) #setando a data da venda como o índice daquela venda para utilizar a func. resample
+vendas_por_mes = vendas_por_mes.resample("ME").sum() #agrupa os dados diários em seus respectivos meses e soma os resultados 
+vendas_por_mes = vendas_por_mes.rename_axis("Mês").reset_index() #renomeando a coluna "data_pedido" por "Mês"
+
+vendas_por_mes["Mês"] = vendas_por_mes["Mês"].dt.strftime("%b") #transforma o retorno padrão (que seria a data do último dia do mês) em abreviação do mês (30 Abril -> Apr)
+vendas_por_mes["vendas"] = (vendas_por_mes["vendas"] / 1e3).round(2) #transformando os dados em milhares 
+#print(vendas_por_mes)
+
+#criando gráfico de linhas
+def grafico_vendas_por_mes(df, filename):
+    #                            larg, alt qualid. da imagem 
+    fig, ax = plt.subplots(figsize=(12,6),dpi=100) #área do gráfico
+    
+    #        x do graf   y do graf  lineweight  
+    ax.plot(df["Mês"], df["vendas"], lw = 3, marker = "o") #construção do gráfico de linha
+
+    # personalizando o gráfico
+    ax.set_title('Total de Vendas em 2023', fontsize = 18, loc='left')
+    ax.set_xlabel('Mês')
+    ax.set_ylabel('Vendas (em milhares de reais)')
+    ax.set_frame_on(False)
+    ax.grid(True, color="grey", ls='--')
+
+    # remover todos os ticks do eixo x e y
+    ax.tick_params(axis='both', which='both', length=0)
+    
+    # limitando a área para não distorcer o gráfico (de 0 ao maior valor+100)
+    plt.ylim(0, df["vendas"].max() + 100)
+
+    plt.savefig(filename, dpi=100, bbox_inches='tight', pad_inches=0)
+    plt.show()
+grafico_vendas_por_mes(vendas_por_mes, "vendas_por_mes.png")
+
+
+
+#criando uma tabela para informar o lucro de cada departamento
+lucro_dpt = vendas[["departamento","lucro"]]
+
+lucro_dpt = lucro_dpt.groupby("departamento").sum() #agrupando os departamentos separadamente e somando cada um
+lucro_dpt = lucro_dpt.reset_index() #limpando índice
+#print(lucro_dpt)
+
+#criando gráfico de barras horizontais
+def grafico_lucro_por_departamento(df, filename):
+    # área do gráfico
+    fig, ax = plt.subplots(figsize=(12,6),dpi=100)
+    
+    #  criar gráfico de colunas horizontais
+    ax.barh(df["departamento"], df["lucro"])
+
+    # personalizando o gráfico
+    ax.set_title('Lucro por Departamento em 2023', fontsize = 18, loc='left')
+    ax.set_frame_on(False)
+    ax.set_xticklabels([]) #remove os valores do eixo x
+
+    # remover todos os ticks do eixo x e y
+    ax.tick_params(axis='both', which='both', length=0)
+
+    # add os valores nas barras
+    for i, v in enumerate(df["lucro"]):
+        ax.text(df["lucro"][i] + 1e3, i, f'R$ {df["lucro"][i]:,.2f}', fontsize = 12, ha = 'left', va='center')
+
+    plt.savefig(filename, dpi=100, bbox_inches='tight', pad_inches=0)
+    plt.show()
+grafico_lucro_por_departamento(lucro_dpt, "lucro_dpt.png")
+
+
+
+# criando um df com os dados desejados (o modo de envio somente do estado de São Paulo)
+envio_sp = vendas.query('estado == "São Paulo"')[["modo_envio"]]
+
+# contando a quantidade de cada modo de envio
+envio_sp = envio_sp.value_counts().to_frame()
+envio_sp = envio_sp.reset_index()
+#print(envio_sp)
+
+#criando gráfico de barras verticais
+def grafico_modo_envio(df, filename):
+    # Área do gráfico
+    fig, ax = plt.subplots(figsize=(12,6),dpi=100)
+
+    ax.bar(df["modo_envio"], df["count"])
+
+    ## Personalizando o gráfico
+    ax.set_title('Modos de Envio mais utilizados em São Paulo (2023)', fontsize = 18, loc='left')
+    ax.set_frame_on(False)
+    ax.set_yticklabels([])
+
+    # remover todos os ticks do eixo x e y
+    ax.tick_params(axis='both', which='both', length=0)
+
+    # Valores nas barras
+    for i, v in enumerate(df["count"]):
+        ax.text(i, df["count"][i], df["count"][i], fontsize = 12, ha = 'center', va='bottom')
+
+    plt.savefig(filename, dpi=100, bbox_inches='tight', pad_inches=0)
+    plt.show()
+grafico_modo_envio(envio_sp, "envio_sp.png")
+
+
+
+dfi.export(vendas_por_mes, "tab_vendas_por_mes.png", table_conversion="matplotlib")
+dfi.export(lucro_dpt, "tab_lucro_dpt.png", table_conversion="matplotlib")
+dfi.export(envio_sp, "tab_envio_sp.png", table_conversion="matplotlib") 
